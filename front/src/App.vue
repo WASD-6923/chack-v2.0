3f1f8c7406988cd38bed0b2101028acbe8c62e06
kiril-i <k2000ivanov@mail.ru>
2025-02-14 14:50:28 +0500

    добавил потенциальный дизайн

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,20 +1,43 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
+
+
 </script>
 
 <template>
 <div>
-  
-  <h2>Регистрация</h2>
+  <div class="welcome">
+  <h2>Добро пожаловать на окно регистрации <br> lorem ipsum! </h2>
+    <p>Пожалуйста, введите свои данные</p>
+  </div>
   <form class="form" action="">
-    <input  placeholder="Почта" type="email">
-    <input  placeholder="Пароль" type="password">
-    <button>Зарегистрироваться</button>
+    <div class="form_div">
+      <input  type="text" placeholder="Имя Пользователя">
+      <input  placeholder="Почта" type="email">
+      <input  placeholder="Пароль" type="password">
+      <button>Зарегистрироваться</button>
+    </div>
   </form>
 </div>
 </template>
 
 <style scoped>
+.welcome {
+  margin-bottom: 300px;
+  background:  #006b1b;
+  padding: 20px;
+  border-radius: 9px;
+}
+
+form {
+  background-color: ;
+  display: block;
+  margin-top: 15px;
+  width: 300px;
+  margin-left: 100px;
+  height: 150px;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
